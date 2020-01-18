c2d7b40d911ba60b7892d8749540fb8991bb0d6e
Topi Kurtelius <topi.kurtelius@nokia.com>
2020-01-18 19:02:08 +0200

    Add some activity to button and correct layout

--- MobCompLab1/app/src/main/java/com/example/mobcomplab1/MainActivity.kt
+++ MobCompLab1/app/src/main/java/com/example/mobcomplab1/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.mobcomplab1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun onClick(view: View) {
+            Toast.makeText(this@MainActivity, "Well hello there :)", Toast.LENGTH_SHORT).show()
     }
 }
